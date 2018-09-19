193fe463c850a967d41443eb6a3570eb60b49874
Unknown <lycoriis.radiata@gmail.com>
2018-09-19 17:17:20 -0600

    Added Var_Func and Ground Settings

---
+++ DGM1610_Game_Fall_2018/Assets/Scripts/Var_Func.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is goofy code aaah
+
+public class Var_Func : MonoBehaviour {
+
+    //Player Move Variables
+    
+    public int MoveSpeed;
+    private int JumpHeight; 
+
+    //Player Grounded Variables
+    
+    private int GroundCheck;
+
+    //Startup Code (Uhhh was i supposed to add anything else or just do this???? Preston helppp)
+
+
+}
+
+
+
