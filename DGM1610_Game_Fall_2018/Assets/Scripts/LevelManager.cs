4f752d118cd89ef81878afb0f1efa97fbe01618f
Unknown <lycoriis.radiata@gmail.com>
2018-10-03 17:16:32 -0600

    Updated LevelManager

--- DGM1610_Game_Fall_2018/Assets/Scripts/LevelManager.cs
+++ DGM1610_Game_Fall_2018/Assets/Scripts/LevelManager.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D Player;
+
+	//Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	//Respawn Delay
+	public float RespawnDelay;
+
+	//Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	//Store Gravity Value
+	private float GravityStore;
+
+	//Use this for initialization
+	void Start () {
+		player = FindObjectofType<Rigidbody2D> ();
 	}
+
+		public void RespawnPlayer(){
+			StartCorouting ("RespawnPlayerCo");
+		}
+
+		public IEnumerator RespawnPlayerCo(){
+			
+			//Generate Death Partcle
+			Istantiate (deathParticle, player.transform.position, player.transform.rotation);
+
+			//Hide Player
+			//player.enabled = false;
+			player.Getcomponent<renderer> ().enabled = false;
+
+			//Gravity Reset
+			gravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
+			player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+			player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			
+			//Point Penalty
+			ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		}
 }
