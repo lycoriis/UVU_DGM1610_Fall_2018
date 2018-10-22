aff4bbcd95d0ede881827749ba992124267bbdfe
Unknown <lycoriis.radiata@gmail.com>
2018-10-22 16:35:22 -0600

    Added Projectile and Applied Pineapple Sprite to PC

---
+++ DGM1610_Game_Fall_2018/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+	//Shoot Variables
+	public Transform FirePoint;
+	public GameObject Projectile;
+
+	void Start(){
+		Projectile = GameObject.Find("Projectile");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightControl))
+			Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
+		
+	}
+}
