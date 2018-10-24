0035b97733b6972ae39126a17f921c38d66d2782
Unknown <lycoriis.radiata@gmail.com>
2018-10-24 16:29:44 -0600

    Added to Projectile (Unfinished) and First Loop

--- DGM1610_Game_Fall_2018/Assets/Scripts/Projectile.cs
+++ DGM1610_Game_Fall_2018/Assets/Scripts/Projectile.cs
@@ -1,34 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+// public class Projectile : MonoBehaviour {
 
-	public float Speed;
-	public Rigidbody2D PC;
+// 	public float Speed;
+// 	public Rigidbody2D PC;
 
-	public GameObject EnemyDeath;
+// 	public GameObject EnemyDeath;
 
-	public GameObject ProjectileParticle;
+// 	public GameObject ProjectileParticle;
 
-	public int PointsForKill;
+// 	public int PointsForKill;
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 		
-		if(PC.transform.localScale.x < 0)
-			Speed = -Speed;
+// 		if(PC.transform.localScale.x < 0)
+// 			Speed = -Speed;
 
-	}
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
-		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
-	}
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Enemy"){
-			tantiate(EnemyDeath; other.transform.position, other.transform.rotation);
-			troy (other.GameObject)
-		}
-	}
-}
+// 	// Update is called once per frame
+// 	void Update () {
+// 		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+// 	}
+// 	void OnTriggerEnter2D(Collider2D other){
+// 		if(other.tag == "Enemy"){
+// 			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+// 			Destroy (other.GameObject);
+// 			ScoreManager.AddPoints(PointsForKill);
+// 		}
+
+// 		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+// 		Destroy(GameObject);
+// 	}
+// }
