1be22ac50ee5b19aab34eb457cbbf4e58c8d94bc
Unknown <lycoriis.radiata@gmail.com>
2018-11-05 16:54:43 -0700

    Loops and Other

--- DGM1610_Game_Fall_2018/Assets/Homework/Loops.cs
+++ DGM1610_Game_Fall_2018/Assets/Homework/Loops.cs
@@ -12,48 +12,50 @@
 
 // public int TempNum = 10;
 
-// public int PowerLevel = 9000;
+// public int PowerNum = 9000;
 
 // 	// Use this for initialization
 // 	void Start () {
-// 		// for(int i=0; i <= 100; i++){
+// 		for(int i=0; i <= 100; i++){
 
-// 		// 	print(i + "Ants Marching");
-// 		// }
+// 			print(i + "Ants Marching");
+// 		}
 
-// 		// for( int i=0; i <= 100; i++){
-// 		// 	print(i + "Enemies left is " + EnemyNum);
-// 		// }
+// 		for( int i=0; i <= 100; i++){
+// 			print(i + "Enemies left is " + EnemyNum);
+// 		}
 
-        // for(int i=0; i <= 0; i--){
-        //     print (i + "Temperature is" + TempNum)
-        // }
+//         for(int i=0; i <= 0; i--){
+//             print (i + "Temperature is" + TempNum);
+//         }
         
-        //for(int i=0; i >= 9000)
-        //  print (i + "Power Level is" + PowerNum)
-
-// 		// while(Num > 0){
-// 		// 	print("Countdown " + Num);
-// 		// 	Num --;
-// 		// }
+//         for(int i=0; i >= 9000;){
+//             print (i + "Power Level is" + PowerNum);
+//         }
+         
+            
+// 		while(Num > 0){
+// 			print("Countdown " + Num);
+// 			Num --;
+// 		}
 		
-// 		// while( Color == "Red" ){
-// 		// 	print("Color is " + Color);
-// 		// } 
+// 		while( Color == "Red" ){
+// 			print("Color is " + Color);
+// 		} 
 
-        //while(EnemyNum == 40 ){
-        //  print("Number of enemies spotted " + EnemyNum);
-        // }
+//         while(EnemyNum == 40 ){
+//          print("Number of enemies spotted " + EnemyNum);
+//         }
 
-        //while(PowerLevel == 9000){
-        // print("Your power level is currently over " + PowerLevel);
-        //}
+//         while(PowerNum == 9000){
+//         print("Your power level is currently over " + PowerNum);
+//         }
 
 		
 			
 	
 // 	// Update is called once per frame
-// 	void Update () {
+// 	// void Update () {
 		
 // 	}
 // }
--- DGM1610_Game_Fall_2018/Assets/Scripts/Switches.cs
+++ DGM1610_Game_Fall_2018/Assets/Scripts/Switches.cs
@@ -1,120 +1,120 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Switches : MonoBehaviour {
+// public class Switches : MonoBehaviour {
 
-	public int num;
-	public string name;
-	public float myFloat;
-	public bool cool;
+// 	// public int num;
+// 	// public string name;
+// 	// public float myFloat;
+// 	// public bool cool;
 	
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 
-		// myFloat == .50f;
+// 		// myFloat == .50f;
 
-		//INT SWITCH
+// 		//INT SWITCH
 
-		// if(num == 1)
-		// 	print("Hurray you picked 1!");
-		// }
+// 		// if(num == 1)
+// 		// 	print("Hurray you picked 1!");
+// 		// }
 
-		// else if(num == 2){
-		// 	}
+// 		// else if(num == 2){
+// 		// 	}
 	
-		// else{
-		// 	print("Boo! You didn't pick 1!")
-		// }
+// 		// else{
+// 		// 	print("Boo! You didn't pick 1!")
+// 		// }
 			
 		
-		//NUM SWITCH
+// 		//NUM SWITCH
 
-		// switch(num){
+// 		// switch(num){
 
-		// 	case 1: 
-		// 		print("You picked " + num );
-		// 		break;
-		// 	case 3: 
-		// 		print("You picked " + num );
-		// 		break;
-		// 	case 6: 
-		// 		print("You picked " + num );
-		// 		break;
-		// 	case 10: 
-		// 		print("You picked " + num );
-		// 		break;
+// 		// 	case 1: 
+// 		// 		print("You picked " + num );
+// 		// 		break;
+// 		// 	case 3: 
+// 		// 		print("You picked " + num );
+// 		// 		break;
+// 		// 	case 6: 
+// 		// 		print("You picked " + num );
+// 		// 		break;
+// 		// 	case 10: 
+// 		// 		print("You picked " + num );
+// 		// 		break;
 
-		// 	default:
-		// 		print("I dont understand" + num);
-		// 		break;
-		// }
+// 		// 	default:
+// 		// 		print("I dont understand" + num);
+// 		// 		break;
+// 		// }
 
-		//STRING SWITCH
+// 		//STRING SWITCH
 		
-		// switch(name){
-		// 	case "Jason":
-		// 		print("Welcome to Camp Crystal Lake " + name + " - love mother"); 
-		// 	break;
+// 		// switch(name){
+// 		// 	case "Jason":
+// 		// 		print("Welcome to Camp Crystal Lake " + name + " - love mother"); 
+// 		// 	break;
 
-		// 	case "Michael":
-		// 		print("Welcome to Haddonfield, Illinois " + name);
-		// 	break;
+// 		// 	case "Michael":
+// 		// 		print("Welcome to Haddonfield, Illinois " + name);
+// 		// 	break;
 
-		// 	case "Freddie":
-		// 		print("Welcome to Elm Street " + name);
-		// 	break;
+// 		// 	case "Freddie":
+// 		// 		print("Welcome to Elm Street " + name);
+// 		// 	break;
 
-		// 	case "Leatherface":
-		// 		print("The stars and stripes are big at night...Deep in the heart of Texas ");
-		// 	break;
+// 		// 	case "Leatherface":
+// 		// 		print("The stars and stripes are big at night...Deep in the heart of Texas ");
+// 		// 	break;
 
-		// 	default:
-		// 		print("I dont know " + name);
-		// 	break;
-		// }
+// 		// 	default:
+// 		// 		print("I dont know " + name);
+// 		// 	break;
+// 		// }
 
-		//FLOAT SWITCH
+// 		//FLOAT SWITCH
 
-		// switch(myFloat){
-		// 	case .60f:
-		// 		print("Flea is " + myFloat + " feet in the air ");
-		// 	break;
+// 		// switch(myFloat){
+// 		// 	case .60f:
+// 		// 		print("Flea is " + myFloat + " feet in the air ");
+// 		// 	break;
 
-		// 	case .70f:
-		// 		print("Flea is ready for the circus. Flea is " + myFloat + " feet in the air ");
-		// 	break;
+// 		// 	case .70f:
+// 		// 		print("Flea is ready for the circus. Flea is " + myFloat + " feet in the air ");
+// 		// 	break;
 
-		// 	case 1.5f:
-		// 		print("Flea has reached a max jump height of " + myFloat + " feet in the air ");
-		// 	break;
+// 		// 	case 1.5f:
+// 		// 		print("Flea has reached a max jump height of " + myFloat + " feet in the air ");
+// 		// 	break;
 
-		// 	default:
-		// 		print("The height" + myFloat + " does not meet circus requirements ");
-		// 	break;
-		// }
+// 		// 	default:
+// 		// 		print("The height" + myFloat + " does not meet circus requirements ");
+// 		// 	break;
+// 		// }
 
-		//BOOL SWITCH 
+// 		//BOOL SWITCH 
 
-		if(cool){
-		print("You're cool!");
-		}
+// 		// if(cool){
+// 		// print("You're cool!");
+// 		// }
 
-		else{ 
-			print ("Boo! You're lame...");
-		}
+// 		// else{ 
+// 		// 	print ("Boo! You're lame...");
+// 		}
 			
 		
 		 
 		
 			
 
-		// }
-	}
+// 		// }
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
+// 	// Update is called once per frame
+// 	void Update () {
 		
-	}
-}
+// 	}
+// }
