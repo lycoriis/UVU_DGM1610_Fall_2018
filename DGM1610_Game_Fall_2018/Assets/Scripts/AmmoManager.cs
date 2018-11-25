af4375fecd54337902774c984ff31c8024d7d6f2
Unknown <lycoriis.radiata@gmail.com>
2018-11-25 16:00:01 -0700

    Attempting to Clone
    Preston I may have goofed but it looks like it worked so idk man??

---
+++ DGM1610_Game_Fall_2018/Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AmmoManager : MonoBehaviour {
+	
+	public static int Ammo;
+
+	Text AmmoText;
+
+	// Use this for initialization
+	void Start () {
+		AmmoText = GetComponent<Text>();
+
+		Ammo = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Ammo < 0)
+			Ammo = 0;
+		AmmoText.text = " " + Ammo;
+	}
+	
+	public static void AddPoints (int PointsToAdd) {
+		Ammo += PointsToAdd;
+	}
+	
+}
---
+++ DGM1610_Game_Fall_2018/Assets/Scripts/HealthValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthValue : MonoBehaviour {
+
+	public const int fullHealth = 100;
+	public int currentHealth = fullHealth;
+
+	public void WasDamage(int amount){
+
+		currentHealth -= amount;
+
+		if (currentHealth <= 0){
+		
+		currentHealth = 0;
+		Debug.Log("Too Bad! Player Dead!");
+		}
+	}
+}
