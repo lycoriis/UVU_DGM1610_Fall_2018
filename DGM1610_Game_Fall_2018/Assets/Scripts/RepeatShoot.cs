ec9243db1651c3fd11cfbcc9cbce935cbd2aab1a
Unknown <lycoriis.radiata@gmail.com>
2018-11-25 18:52:00 -0700

    Finished Billboard. Added RepeatShoot
    Using StackExchange walkthrough of delayed and continuous fire

--- DGM1610_Game_Fall_2018/Assets/Scripts/RepeatShoot.cs
+++ DGM1610_Game_Fall_2018/Assets/Scripts/RepeatShoot.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Billboard : MonoBehaviour {
+public class RepeatShoot : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(Camera.main.transform);
 		
 	}
 }
